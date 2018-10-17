a00f52e5a5ee6a4005aabff6b782ee5f3e257500
Baxter Lane <baxterwlane@outlook.com>
2018-10-17 14:00:37 -0500

    test in, of one controller, renamed things again, good to go I think

---
+++ CroweCSharpTest.GreetingsToEarth.Website.Tests/Controllers/GreetingControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using CroweCSharpTest.GreetingsToEarthWebsite.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class GreetingControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            GreetingController controller = new GreetingController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+      
+    }
+}
